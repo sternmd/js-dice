61a04a5ece30b65e4d94e5956960f74bf64ae9d2
Max Stern <stern.maxwell@gmail.com>
2018-06-09 14:31:57 -0500

    event handling

--- app.js
+++ app.js
@@ -9,18 +9,26 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
-activePlayer = 0; // 0 is first player, 1 is second player
-
-dice = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
-
-
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
+activePlayer = 1; // 0 is first player, 1 is second player
 
 document.querySelector('.dice').style.display = 'none';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+  // 1. Random number
+  var dice = Math.floor(Math.random() * 6) + 1;
+
+  // 2. Display result
+  var diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+  diceDOM.src = 'dice-' + dice + '.png';
+
+  // 3. Update round score, IF rolled number was NOT a 1
+})
